844a73814d64c50efcad5cb9c5cca2d0194fe86a
cj5 <stenny.cj@hotmail.com>
2016-07-26 23:05:19 -0500

    added parallax to landing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+    function updateContainer() {
+        var theHeight = $(window).height();
+        var titleWrapperHeight = $('.title-wrapper').height();
+        var titlePadding = theHeight/2-titleWrapperHeight/2;
+        var navHeight = $('.main-nav ul').height(); 
+        var scroll = $(window).scrollTop();
+        
+        //makes landing page full viewport height
+        $('.landing').css('height', theHeight);
+        $('.bg').css('height', theHeight);
+        $('.title-wrapper').css('margin-top', titlePadding-navHeight*3);
+        $('.title-wrapper').css('margin-bottom', titlePadding);
+        
+        $('.main-nav').css('padding-top', navHeight);
+        $('.main-nav').css('padding-bottom', navHeight);
+
+        $('.artists').css('top', theHeight);
+
+        console.log('navHeight', navHeight);
+    } 
+    
+    updateContainer();
+
+    $(window).resize(function() {
+        updateContainer();
+    });
+
+});    

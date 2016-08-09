7187a8102ae893dd7fd2145fc0020c7675928074
cj5 <stenny.cj@hotmail.com>
2016-08-08 22:32:15 -0500

    added all sections html/css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
 
     //creates a smooth scroll when any a link is clicked
-    // $('#artists').click(function(){
+    // $('a').click(function(){
     //     $('html, body').animate({
-    //         scrollTop: $( $.attr(this, 'href') ).offset().top
+    //         scrollTop: $($.attr(this, 'href')).offset().top
     //     }, 500);
     //     return false;
+    // });
+
+
+    // 'catTopPosition' is the amount of pixels #cat
+    // is from the top of the document
+    // var artistsTopPosition = $('#artists').offset().top;
+    
+    // // When #scroll is clicked
+    // $('a#artists').click(function(){
+    //     // Scroll down to 'catTopPosition'
+    //     $('html, body').animate({scrollTop:artistsTopPosition}, 500);
+    //     // Stop the link from acting like a normal anchor link
+    //     return false;
+    // });
+
+
+    // $(function() {
+    //     $('a').click(function() {
+    //         $('html,body').animate({'artists' : 0},1000);
+    //     });
     // });
 
     function updateContainer() {
@@ -17,6 +37,8 @@ $(document).ready(function(){
         
         //makes landing page full viewport height
         $('.landing').css('height', theHeight);
+        $('.bg-color-filter').css('height', theHeight);
+        $('.bg-color-filter-2').css('height', theHeight);
         $('.bg').css('height', theHeight);
         $('.title-wrapper').css('margin-top', titlePadding-navHeight*3);
         $('.title-wrapper').css('margin-bottom', titlePadding);
@@ -34,7 +56,5 @@ $(document).ready(function(){
     $(window).resize(function() {
         updateContainer();
     });
-
 }); 
-
 

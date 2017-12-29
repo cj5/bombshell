488517eb79cda1af0db8a2ee93b37cec3356e8ab
Chris <stac1221@gmail.com>
2017-12-29 17:50:05 -0600

    updates artist images, css — edited image layouts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+console.log('test')
+
+$(document).ready(function() {
     $('#navicon').click(function(){
         $(this).toggleClass('open'); //(navicon transition)
         $('.menu').toggleClass('open'); //(adds nav menu)
@@ -8,66 +10,32 @@ $(document).ready(function(){
     $('a.a-mobile').click(function(){
         $('#navicon').toggleClass('open');
         $('.menu').removeClass('open');
-    });      
+    }); 
 
+    // smooth scroll
+    // $('a[href^="#"]').on('click',function (e) {
+    //   e.preventDefault();
+    //   var target = this.hash,
+    //   $target = $(target);
+    //   $('html, body').stop().animate({
+    //     'scrollTop': $target.offset().top
+    //   }, 500, 'swing', function () {
+    //     window.location.hash = target;
+    //   }); 
+    // });
 
-    //creates a smooth scroll when any a link is clicked
-    // $('a').click(function(){
+    // $(document).on('click', 'a[href^="#"]', function (event) {
+    //     event.preventDefault();
+
     //     $('html, body').animate({
     //         scrollTop: $($.attr(this, 'href')).offset().top
     //     }, 500);
-    //     console.log('smooth scroll', this);
-    //     return false;
-    // });
-
-    // $('a').click(function(){
-    //     $('html, body').animate({
-    //         scrollTop: $("#").offset().top
-    //     }, 2000);
-    //     return false;
-    // });
-
-    // $('a[href^="#"]').on('click',function (e) {
-    //     e.preventDefault();
-
-    //     var target = this.hash;
-    //     var $target = $(target);
-
-    //     $('html, body').stop().animate({
-    //         'scrollTop': $target.offset().top
-    //     }, 900, 'swing', function () {
-    //         window.location.hash = target;
-    //     });
-    // });
-
-    // $("a").click(function(event) {
-    //     event.preventDefault();
-    //     $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
-    //     });
-
-    // 'catTopPosition' is the amount of pixels #cat
-    // is from the top of the document
-    // var artistsTopPosition = $('#artists').offset().top;
-    
-    // // When #scroll is clicked
-    // $('a#artists').click(function(){
-    //     // Scroll down to 'catTopPosition'
-    //     $('html, body').animate({scrollTop:artistsTopPosition}, 500);
-    //     // Stop the link from acting like a normal anchor link
-    //     return false;
-    // });
-
-
-    // $(function() {
-    //     $('a').click(function() {
-    //         $('html,body').animate({'artists' : 0},1000);
-    //     });
     // });
 
     function updateContainer() {
         var theHeight = $(window).height();
         var titleWrapperHeight = $('.title-wrapper').height();
-        var titlePadding = theHeight/2-titleWrapperHeight/2;
+        var titlePadding = theHeight / 2 - titleWrapperHeight / 2;
         var navHeight = $('.main-nav ul').height(); 
         var scroll = $(window).scrollTop();
         
@@ -76,26 +44,13 @@ $(document).ready(function(){
         $('.bg-color-filter').css('height', theHeight);
         $('.bg-color-filter-2').css('height', theHeight);
         $('.bg').css('height', theHeight);
-        $('.title-wrapper').css('margin-top', titlePadding-navHeight*3);
+        $('.title-wrapper').css('margin-top', titlePadding - navHeight * 3);
         $('.title-wrapper').css('margin-bottom', titlePadding);
         
         $('.main-nav').css('padding-top', navHeight);
         $('.main-nav').css('padding-bottom', navHeight);
 
-        $('.artists').css('top', theHeight);
-
-        // if ($(window).innerWidth() <= 753) {
-        //     $('.a').addClass('display-none');
-        // // } else {
-            
-        // }
-
-        // if ($(window).innerWidth() <= 753){
-        //         menu.className = 'display-none';
-        //     } else {
-        //         menu.className ='display';
-        //     }
-           
+        $('.artists').css('top', theHeight);           
 
         console.log('navHeight', navHeight);
     } 
@@ -106,4 +61,11 @@ $(document).ready(function(){
         updateContainer();
     });
 }); 
+
+// $('a').click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+//     return false;
+// }); 
 
